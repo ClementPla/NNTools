b49e3c134909b70564ec5a7bb8e39ed41eaea576
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2020-10-08 18:36:35 -0400

    bug fixing

--- nntools/nnet/models/deeplabv3.py
+++ nntools/nnet/models/deeplabv3.py
@@ -78,10 +78,65 @@ class Decoder(nn.Module):
         x = torch.cat((x, decoder_f), dim=1)
         return self.classifier(x)
 
+model_urls = {
+    'fcn_resnet50_coco': 'https://download.pytorch.org/models/fcn_resnet50_coco-1167a1af.pth',
+    'fcn_resnet101_coco': 'https://download.pytorch.org/models/fcn_resnet101_coco-7ecb50ca.pth',
+    'deeplabv3_resnet50_coco': 'https://download.pytorch.org/models/deeplabv3_resnet50_coco-cd0a2569.pth',
+    'deeplabv3_resnet101_coco': 'https://download.pytorch.org/models/deeplabv3_resnet101_coco-586e9e4e.pth',
+}
+
+from torchvision.models import resnet
+from torchvision.models._utils import IntermediateLayerGetter
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead, DeepLabV3
+from torchvision.models.utils import load_state_dict_from_url
+
+
+def _segm_resnet(name, backbone_name, num_classes, pretrained_backbone=True):
+    backbone = resnet.__dict__[backbone_name](
+        pretrained=pretrained_backbone,
+        replace_stride_with_dilation=[False, True, True])
+
+    return_layers = {'layer4': 'out', 'layer1': 'features'}
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    model_map = {
+        'deeplabv3': (DeepLabHead, DeepLabV3),
+    }
+    inplanes = 2048
+    classifier = model_map[name][0](inplanes, num_classes)
+    base_model = model_map[name][1]
+
+    model = base_model(backbone, classifier, aux_classifier)
+    return model
+
+
+def _load_model(arch_type, backbone, pretrained, progress, num_classes, **kwargs):
+    model = _segm_resnet(arch_type, backbone, num_classes, **kwargs)
+    if pretrained:
+        arch = arch_type + '_' + backbone + '_coco'
+        model_url = model_urls[arch]
+        if model_url is None:
+            raise NotImplementedError('pretrained {} is not supported as of now'.format(arch))
+        else:
+            state_dict = load_state_dict_from_url(model_url, progress=progress)
+            model.load_state_dict(state_dict, strict=False)
+    return model
+
+
+def deeplabv3_resnet101(pretrained=True, progress=True,
+                        num_classes=21, **kwargs):
+    """Constructs a DeepLabV3 model with a ResNet-101 backbone.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on COCO train2017 which
+            contains the same classes as Pascal VOC
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _load_model('deeplabv3', 'resnet101', pretrained, progress, num_classes, **kwargs)
+
 
 class Encoder(nn.Module):
     def __init__(self, config):
-        from torchvision.models.segmentation import deeplabv3_resnet101
         super(Encoder, self).__init__()
         self.config = config
 
@@ -120,6 +175,6 @@ class Encoder(nn.Module):
         result = self.encoder(x)
         x = self.aspp(result['out'])
         if features:
-            return x, result['aux']
+            return x, result['features']
         else:
             return x
