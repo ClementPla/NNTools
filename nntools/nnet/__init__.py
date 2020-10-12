a0a16f125db2c577891fe1998fc725a94f922022
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2020-10-12 11:37:48 -0400

    Network proxy

---
+++ nntools/nnet/__init__.py
@@ -0,0 +1,1 @@
+from .utils import nnt_format
--- nntools/nnet/utils.py
+++ nntools/nnet/utils.py
@@ -1,8 +1,21 @@
 import torch.nn as nn
-
+from .abstract_nn import AbstractNet
 
 def norm_layer(norm):
     return {'batch_norm': nn.BatchNorm2d,
             'sync_batch_norm': nn.SyncBatchNorm,
             'instance_norm': nn.InstanceNorm2d,
             'layer_norm': nn.LayerNorm}[norm]
+
+
+class ProxyNetwork(AbstractNet):
+    def __init__(self, model):
+        super(ProxyNetwork, self).__init__()
+        self.proxy = model
+
+    def forward(self, *args, **kwargs):
+        return self.proxy(*args, **kwargs)
+
+
+def nnt_format(model):
+    return ProxyNetwork(model)
