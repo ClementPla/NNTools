e5a5b93d726e4596b6c4a9fec0f34a71a1f08e94
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-01-18 07:40:26 -0500

    Add proxy import in dataset.__init__

--- nntools/dataset/__init__.py
+++ nntools/dataset/__init__.py
@@ -3,4 +3,4 @@ from nntools.dataset.seg_dataset import SegmentationDataset
 from nntools.dataset.classif_dataset import ClassificationDataset
 
 from nntools.dataset.tools import Composition, DataAugment
-from nntools.dataset.utils import get_segmentation_class_count, class_weighting, random_split
+from nntools.dataset.utils import get_classification_class_count, get_segmentation_class_count, class_weighting, random_split
