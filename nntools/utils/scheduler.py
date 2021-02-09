e1114936297f9056447fdcc0bf2b444b30763f50
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-02-08 21:02:53 -0500

    Refactor the optimizers and schedulers

--- nntools/utils/scheduler.py
+++ nntools/utils/scheduler.py
@@ -1,21 +1,32 @@
 import torch
 
+
+def identity(*args):
+    return args
+
+
+def empty(*args):
+    pass
+
+
+def continuous_epoch(epoch, iteration, epoch_size):
+    return epoch + iteration / epoch_size
+
+
+def discrete_epoch(epoch, iteration, epoch_size):
+    return epoch
+
+
 SCHEDULER = {
-    'LambdaLR': (torch.optim.lr_scheduler.LambdaLR, 'on_epoch', lambda epoch, iteration, epoch_size: epoch),
-    'MultiplicativeLR': (
-    torch.optim.lr_scheduler.MultiplicativeLR, 'on_epoch', lambda epoch, iteration, epoch_size: epoch),
-    'StepLR': (torch.optim.lr_scheduler.StepLR, 'on_epoch', lambda epoch, iteration, epoch_size: epoch),
-    'MultiStepLR': (torch.optim.lr_scheduler.MultiStepLR, 'on_epoch', lambda epoch, iteration, epoch_size: epoch),
-    'ExponentialLR': (torch.optim.lr_scheduler.ExponentialLR, 'on_epoch', lambda epoch, iteration, epoch_size: epoch),
-    'CosineAnnealingLR': (torch.optim.lr_scheduler.CosineAnnealingLR, 'on_epoch', lambda epoch, iteration,
-                                                                                         epoch_size:
-    epoch + iteration / epoch_size),
-    'ReduceLROnPlateau': (torch.optim.lr_scheduler.ReduceLROnPlateau, 'on_validation', lambda x: x),
-    'CyclicLR': (torch.optim.lr_scheduler.CyclicLR, 'on_iteration', lambda epoch, iteration,
-                                                                           epoch_size: None),
-    'OneCycleLR': (torch.optim.lr_scheduler.OneCycleLR, 'on_iteration', lambda epoch, iteration,
-                                                                               epoch_size: None),
+    'LambdaLR': (torch.optim.lr_scheduler.LambdaLR, 'on_epoch', empty),
+    'MultiplicativeLR': (torch.optim.lr_scheduler.MultiplicativeLR, 'on_epoch', empty),
+    'StepLR': (torch.optim.lr_scheduler.StepLR, 'on_epoch', empty),
+    'MultiStepLR': (torch.optim.lr_scheduler.MultiStepLR, 'on_epoch', empty),
+    'ExponentialLR': (torch.optim.lr_scheduler.ExponentialLR, 'on_epoch', empty),
+    'CosineAnnealingLR': (torch.optim.lr_scheduler.CosineAnnealingLR, 'on_epoch', continuous_epoch),
+    'ReduceLROnPlateau': (torch.optim.lr_scheduler.ReduceLROnPlateau, 'on_validation', identity),
+    'CyclicLR': (torch.optim.lr_scheduler.CyclicLR, 'on_iteration', identity),
+    'OneCycleLR': (torch.optim.lr_scheduler.OneCycleLR, 'on_iteration', identity),
     'CosineAnnealingWarmRestarts': (
-    torch.optim.lr_scheduler.CosineAnnealingWarmRestarts, 'on_iteration', lambda epoch, iteration,
-                                                                                 epoch_size: None)
+        torch.optim.lr_scheduler.CosineAnnealingWarmRestarts, 'on_iteration', identity)
 }
