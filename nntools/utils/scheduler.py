566dc7b10d8745d94259c224bba7a6d4eb3f8c7a
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-02-09 16:37:17 -0500

    Try a work around to allow pickle on namedtuples

--- nntools/utils/scheduler.py
+++ nntools/utils/scheduler.py
@@ -19,17 +19,17 @@ def discrete_epoch(epoch, iteration, epoch_size):
 
 from collections import namedtuple
 
-scheduler = namedtuple('Scheduler', ['func', 'call_on', 'callback'])
+Scheduler = namedtuple('Scheduler', ['func', 'call_on', 'callback'])
 SCHEDULERS = {
-    'LambdaLR': scheduler(torch.optim.lr_scheduler.LambdaLR, 'on_epoch', empty),
-    'MultiplicativeLR': scheduler(torch.optim.lr_scheduler.MultiplicativeLR, 'on_epoch', empty),
-    'StepLR': scheduler(torch.optim.lr_scheduler.StepLR, 'on_epoch', empty),
-    'MultiStepLR': scheduler(torch.optim.lr_scheduler.MultiStepLR, 'on_epoch', empty),
-    'ExponentialLR': scheduler(torch.optim.lr_scheduler.ExponentialLR, 'on_epoch', empty),
-    'CosineAnnealingLR': scheduler(torch.optim.lr_scheduler.CosineAnnealingLR, 'on_epoch', continuous_epoch),
-    'ReduceLROnPlateau': scheduler(torch.optim.lr_scheduler.ReduceLROnPlateau, 'on_validation', identity),
-    'CyclicLR': scheduler(torch.optim.lr_scheduler.CyclicLR, 'on_iteration', identity),
-    'OneCycleLR': scheduler(torch.optim.lr_scheduler.OneCycleLR, 'on_iteration', identity),
-    'CosineAnnealingWarmRestarts': scheduler(
+    'LambdaLR': Scheduler(torch.optim.lr_scheduler.LambdaLR, 'on_epoch', empty),
+    'MultiplicativeLR': Scheduler(torch.optim.lr_scheduler.MultiplicativeLR, 'on_epoch', empty),
+    'StepLR': Scheduler(torch.optim.lr_scheduler.StepLR, 'on_epoch', empty),
+    'MultiStepLR': Scheduler(torch.optim.lr_scheduler.MultiStepLR, 'on_epoch', empty),
+    'ExponentialLR': Scheduler(torch.optim.lr_scheduler.ExponentialLR, 'on_epoch', empty),
+    'CosineAnnealingLR': Scheduler(torch.optim.lr_scheduler.CosineAnnealingLR, 'on_epoch', continuous_epoch),
+    'ReduceLROnPlateau': Scheduler(torch.optim.lr_scheduler.ReduceLROnPlateau, 'on_validation', identity),
+    'CyclicLR': Scheduler(torch.optim.lr_scheduler.CyclicLR, 'on_iteration', identity),
+    'OneCycleLR': Scheduler(torch.optim.lr_scheduler.OneCycleLR, 'on_iteration', identity),
+    'CosineAnnealingWarmRestarts': Scheduler(
         torch.optim.lr_scheduler.CosineAnnealingWarmRestarts, 'on_iteration', identity)
 }
