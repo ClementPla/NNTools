80f42e5ef9e8b4bdd0e81ac8ec259ccb7beff1c5
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-08-15 16:56:06 +0200

    re-design nntools_wrapper to allow default argument to be passed

--- nntools/dataset/image_tools.py
+++ nntools/dataset/image_tools.py
@@ -7,15 +7,17 @@ import numpy as np
 
 def nntools_wrapper(func):
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        list_parameters = inspect.signature(func).parameters.values()
-        params_name = ([p.name for p in list_parameters])
-
-        arguments = {k: kwargs.pop(k) for k in params_name if k in kwargs}
+    def wrapper(**kwargs):
+        expected_parameters = inspect.signature(func).parameters.values()
+        arguments = {}
+        for p in expected_parameters:
+            if p.name in kwargs:
+                arguments[p.name] = kwargs.pop(p.name)
+            elif p.default is not p.empty:
+                arguments[p.name] = p.default
         output = func(**arguments)
         output.update(kwargs)
         return output
-
     return wrapper
 
 
@@ -32,3 +34,4 @@ def resize(image, keep_size_ratio=True, shape=(512, 512), flag=cv2.INTER_LINEAR)
 
     image = cv2.resize(image, dsize=shape, interpolation=flag)
     return image
+
