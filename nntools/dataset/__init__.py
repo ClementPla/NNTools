dc67a1e2d7ea77ef894b9b7f88d93d779fab18e9
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-10-13 13:00:45 +0200

    Add implementation of class-sample counting in the classification Dataset

--- nntools/dataset/__init__.py
+++ nntools/dataset/__init__.py
@@ -3,5 +3,5 @@ from nntools.dataset.image_tools import nntools_wrapper
 from nntools.dataset.multi_image_dataset import MultiImageDataset
 from nntools.dataset.seg_dataset import SegmentationDataset
 from nntools.dataset.tools import Composition
-from nntools.dataset.utils import get_classification_class_count, get_segmentation_class_count, class_weighting, \
+from nntools.dataset.utils import get_segmentation_class_count, class_weighting, \
     random_split
