4bf19f139c5d79c69d7f261eb689a4f937398ea3
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2022-01-18 21:48:31 +0100

    Change the plotting option from the dataset to make it an external function

--- nntools/dataset/image_dataset.py
+++ nntools/dataset/image_dataset.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import tqdm
-from matplotlib import cm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 from torch.utils.data import Dataset
 
 from nntools.dataset.image_tools import resize
 from nntools.utils.io import read_image
+from nntools.utils.plotting import plot_images
 from nntools.utils.misc import to_iterable, identity
 from nntools.tracker.logger import Log
 supportedExtensions = ["jpg", "jpeg", "png", "tiff", "tif", "jp2", "exr", "pbm", "pgm", "ppm", "pxm", "pnm"]
@@ -214,52 +214,8 @@ class ImageDataset(Dataset):
 
     def plot(self, item, classes=None, fig_size=1):
         arrays = self.__getitem__(item, torch_cast=False, transpose_img=False, return_indices=False)
-
-        arrays = [(k, v) for k, v in arrays.items() if isinstance(v, np.ndarray)]
-        nb_plots = len(arrays)
-        row, col = int(math.ceil(nb_plots / 2)), 2
-
-        fig, ax = plt.subplots(row, col)
-        if row == 1:
-            ax = [ax]
-        fig.set_size_inches(fig_size * 10, 5 * row * fig_size)
-
-        for i in range(row):
-            for j in range(col):
-                ax[i][j].set_axis_off()
-
-                if j + i * col >= len(arrays):
-                    ax[i][j].imshow(np.zeros_like(np.squeeze(arr)), interpolation='none')
-                else:
-                    name, arr = arrays[j + i * col]
-
-                    ax[i][j].set_title(name)
-                    if arr.ndim == 3 and arr.shape[-1] > 3:
-                        arr_tmp = np.argmax(arr, axis=-1) + 1
-                        arr_tmp[arr.max(axis=-1) == 0] = 0
-                        arr = arr_tmp
-                    if arr.ndim == 3:
-                        arr = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
-                        ax[i][j].imshow(np.squeeze(arr), cmap='gray', interpolation='none')
-                    elif arr.ndim == 2:
-                        n_classes = np.max(arr) + 1
-                        if n_classes == 1:
-                            n_classes = 2
-                        cmap = cm.get_cmap(self.cmap_name, n_classes)
-                        ax[i][j].imshow(np.squeeze(arr), cmap=cmap, interpolation='none')
-                        divider = make_axes_locatable(ax[i][j])
-                        cax = divider.append_axes('right', size='5%', pad=0.05)
-                        cax.imshow(np.expand_dims(np.arange(n_classes), 0).transpose((1, 0)), aspect='auto', cmap=cmap)
-                        cax.yaxis.set_label_position("right")
-                        cax.yaxis.tick_right()
-                        if classes is not None:
-                            cax.set_yticklabels(labels=classes)
-                        cax.yaxis.set_ticks(np.arange(n_classes))
-                        cax.get_xaxis().set_visible(False)
-
-        fig.tight_layout()
-        fig.show()
-
+        plot_images(arrays, self.cmap_name, classes=classes, fig_size=fig_size)
+        
     def get_mosaic(self, n_items=9, shuffle=False, indexes=None, resolution=(512, 512), show=False, fig_size=1,
                    save=None, add_labels=False,
                    n_row=None, n_col=None):
---
+++ nntools/utils/plotting.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def plot_images(arrays_dict, cmap_name='jet_r', , classes=None, fig_size=1):
+    arrays = [(k, v) for k, v in arrays_dict.items() if isinstance(v, np.ndarray)]
+    nb_plots = len(arrays)
+    row, col = int(math.ceil(nb_plots / 2)), 2
+
+    fig, ax = plt.subplots(row, col)
+    if row == 1:
+        ax = [ax]
+    fig.set_size_inches(fig_size * 10, 5 * row * fig_size)
+
+    for i in range(row):
+        for j in range(col):
+            ax[i][j].set_axis_off()
+
+            if j + i * col >= len(arrays):
+                ax[i][j].imshow(np.zeros_like(np.squeeze(arr)), interpolation='none')
+            else:
+                name, arr = arrays[j + i * col]
+
+                ax[i][j].set_title(name)
+                if arr.ndim == 3 and arr.shape[-1] > 3:
+                    arr_tmp = np.argmax(arr, axis=-1) + 1
+                    arr_tmp[arr.max(axis=-1) == 0] = 0
+                    arr = arr_tmp
+                if arr.ndim == 3:
+                    arr = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+                    ax[i][j].imshow(np.squeeze(arr), cmap='gray', interpolation='none')
+                elif arr.ndim == 2:
+                    n_classes = np.max(arr) + 1
+                    if n_classes == 1:
+                        n_classes = 2
+                    cmap = cm.get_cmap(cmap_name, n_classes)
+                    ax[i][j].imshow(np.squeeze(arr), cmap=cmap, interpolation='none')
+                    divider = make_axes_locatable(ax[i][j])
+                    cax = divider.append_axes('right', size='5%', pad=0.05)
+                    cax.imshow(np.expand_dims(np.arange(n_classes), 0).transpose((1, 0)), aspect='auto', cmap=cmap)
+                    cax.yaxis.set_label_position("right")
+                    cax.yaxis.tick_right()
+                    if classes is not None:
+                        cax.set_yticklabels(labels=classes)
+                    cax.yaxis.set_ticks(np.arange(n_classes))
+                    cax.get_xaxis().set_visible(False)
+
+    fig.tight_layout()
+    fig.show()
