b884f584f118cef2e413155e6d00e0f0aa7bd567
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2022-02-07 11:29:57 +0100

    Debug print flag

--- nntools/experiment/experiment.py
+++ nntools/experiment/experiment.py
@@ -372,6 +372,7 @@ class Experiment(Manager):
         train_loader, train_sampler = self.get_dataloader(self.train_dataset, drop_last=True, rank=rank)
         for key, value in self.additional_datasets.items():
             self.ctx.additional_dataloader[key] = self.get_dataloader(value, drop_last=True, rank=rank)
+        print('Debug 1-b')
 
         optimizer = self.partial_optimizer(
             model.get_trainable_parameters())
