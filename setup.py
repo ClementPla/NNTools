28f313ea12676b8c155dc5a25beb9408fb3361d9
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-07-06 22:49:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ setup(
     license='MIT',
     author='Clement Playout',
     author_email = 'clement.playout@polymtl.ca',
-    url = 'https://github.com/ClementPla/fundus-lesions-toolkit/tree/main',
-    download_url = 'https://github.com/ClementPla/NNTools/archive/refs/tags/v_011.tar.gz',
+    download_url = 'https://github.com/ClementPla/NNTools/archive/refs/tags/v0.1.0.tar.gz',
     keywords = ['PyTorch', 'Neural Network', 'CNN', 'deep learning', 'training', 'dataset', 'image', 'configuration'],
     author_email = 'clement.playout@polymtl.ca',
     install_requires = [
         'torch', 'numpy','matplotlib','tqdm','pyyaml','pandas', 'torchvision', 'opencv-python',
         'segmentation_models_pytorch', 'bokeh'],
-    description='Small library built to facilitate the:q training of neural network with Pytorch. '
+    description='Light library built to facilitate the training of neural network with Pytorch. '
 )
