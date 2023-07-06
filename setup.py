32d723dd0604ec6ca25c49f8d9c786ea68831bc4
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-07-06 22:47:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='NNTools',
-    version='0.1.1',
-    packages=['nntools', 'nntools.nnet', 'nntools.utils', 'nntools.dataset', 'nntools.utils',
-              'nntools.tracker'],
+    name = 'NNTools',
+    version = '0.1.0',
+    packages = ['nntools'],
     url='https://github.com/ClementPla/NNTools',
     license='MIT',
     author='Clement Playout',
-    download_url='https://github.com/ClementPla/NNTools/archive/refs/tags/v_01.tar.gz',
-    keywords=['pytorch', 'neural network', 'deep learning', 'training', 'dataset', 'image', 'configuration'],
-    author_email='clement.playout@polymtl.ca',
-    install_requires=[
-        'torch','numpy','matplotlib','tqdm','pyyaml','pandas', 'torchvision', 'opencv-python',
+    author_email = 'clement.playout@polymtl.ca',
+    url = 'https://github.com/ClementPla/fundus-lesions-toolkit/tree/main',
+    download_url = 'https://github.com/ClementPla/NNTools/archive/refs/tags/v_011.tar.gz',
+    keywords = ['PyTorch', 'Neural Network', 'CNN', 'deep learning', 'training', 'dataset', 'image', 'configuration'],
+    author_email = 'clement.playout@polymtl.ca',
+    install_requires = [
+        'torch', 'numpy','matplotlib','tqdm','pyyaml','pandas', 'torchvision', 'opencv-python',
         'segmentation_models_pytorch', 'bokeh'],
     description='Small library built to facilitate the:q training of neural network with Pytorch. '
 )
