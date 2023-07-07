fb85b2403725156180d96a158b085cdabaefc527
Zacharie Legault <zacharie@hudsonlegault.net>
2023-07-07 21:53:55 +0000

    Use headless version of OpenCV

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ setup(
     author_email = 'clement.playout@polymtl.ca',
     download_url = 'https://github.com/ClementPla/NNTools/archive/refs/tags/v0.1.0.tar.gz',
     keywords = ['PyTorch', 'Neural Network', 'CNN', 'deep learning', 'training', 'dataset', 'image', 'configuration'],
-    author_email = 'clement.playout@polymtl.ca',
     install_requires = [
-        'torch', 'numpy','matplotlib','tqdm','pyyaml','pandas', 'torchvision', 'opencv-python',
+        'torch', 'numpy','matplotlib','tqdm','pyyaml','pandas', 'torchvision', 'opencv-python-headless',
         'segmentation_models_pytorch', 'bokeh'],
     description='Light library built to facilitate the training of neural network with Pytorch. '
 )
