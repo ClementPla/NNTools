78df6fe75452b383400484d66af84bba0ec00cb4
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-11-05 17:37:21 -0500

    Fix a bug in the format of gts mask in SegmentationDataset (wrong dtype)

--- src/nntools/dataset/seg_dataset.py
+++ src/nntools/dataset/seg_dataset.py
@@ -1,4 +1,5 @@
 import glob
+import logging
 
 import cv2
 import numpy as np
@@ -7,11 +8,13 @@ from nntools import MISSING_DATA_FLAG, NN_FILL_DOWNSAMPLE, NN_FILL_UPSAMPLE
 from nntools.dataset.image_tools import resize
 from nntools.utils.io import path_leaf, read_image
 from nntools.utils.misc import to_iterable
-import logging
+
 from .abstract_image_dataset import AbstractImageDataset, supportedExtensions
+
 
 def extract_filename_without_extension(filename):
     return filename.split(".")[0]
+
 
 class SegmentationDataset(AbstractImageDataset):
     def __init__(
@@ -22,7 +25,7 @@ class SegmentationDataset(AbstractImageDataset):
         keep_size_ratio=False,
         recursive_loading=True,
         n_classes=None,
-        extract_image_id_function=extract_filename_without_extension,
+        extract_image_id_function=None,
         use_cache=False,
         auto_pad=True,
         filling_strategy=NN_FILL_UPSAMPLE,
@@ -35,7 +38,9 @@ class SegmentationDataset(AbstractImageDataset):
             self.path_masks = {"mask": to_iterable(mask_url)}
         else:
             self.path_masks = {k: to_iterable(path) for k, path in mask_url.items()}
-
+        if extract_image_id_function is None:
+            extract_image_id_function = extract_filename_without_extension
+            
         self.use_masks = self.path_masks is not None
         self.n_classes = n_classes
         self.filling_strategy = filling_strategy
@@ -77,18 +82,14 @@ class SegmentationDataset(AbstractImageDataset):
                 argsort_ids = np.argsort(gts_ids[mask_key])
                 gts_ids[mask_key] = np.asarray(gts_ids[mask_key])[argsort_ids]
                 self.gts[mask_key] = self.gts[mask_key][argsort_ids]
-        
+
         self.img_filepath["image"] = np.asarray(self.img_filepath["image"])
         img_ids = np.asarray([self.extract_image_id_function(path_leaf(path)) for path in self.img_filepath["image"]])
         argsort_ids = np.argsort(img_ids)
         img_ids = img_ids[argsort_ids]
         self.img_filepath["image"] = self.img_filepath["image"][argsort_ids]
-        
-        if self.use_masks:
-            masks_filenames = {}
-            for k, files_list in self.gts.items():
-                masks_filenames[k] = [path_leaf(file).split(".")[0] for file in files_list]
 
+        if self.use_masks:
             list_lengths = [len(mask_ids) for mask_ids in gts_ids.values()] + [len(img_ids)]
             all_equal = all(elem == list_lengths[0] for elem in list_lengths)
 
@@ -97,36 +98,40 @@ class SegmentationDataset(AbstractImageDataset):
                     "Mismatch between the size of the different input folders (longer %i, smaller %i)"
                     % (max(list_lengths), min(list_lengths))
                 )
-                logging.debug(f"List lengths: {list(zip(list(masks_filenames.keys())+['image'], list_lengths))}")
+                logging.debug(f"List lengths: {list(zip(list(gts_ids.keys())+['image'], list_lengths))}")
 
             list_common_file = set(img_ids)
             for mask_ids in gts_ids.values():
                 list_common_file = list_common_file & set(mask_ids)
-            
             intersection_ids = np.asarray(list(list_common_file))
             logging.debug(f"Number of files in intersection dataset: {len(intersection_ids)}")
+            if len(intersection_ids) == 0:
+                logging.warning("No common files between the different folders")
+                for k in self.gts.keys():
+                    logging.debug(f"List of files in {k}: {gts_ids[k]}")
+                logging.debug(f"List of files in image: {img_ids}")
+
             if self.filling_strategy == NN_FILL_DOWNSAMPLE or all_equal:
                 # We only keep the intersection of the files
                 if not all_equal:
                     logging.warning("Downsampling the dataset to size %i" % min(list_lengths))
-                    
+
                 self.img_filepath["image"] = self.img_filepath["image"][np.isin(img_ids, intersection_ids)]
 
                 for k in self.gts.keys():
                     self.gts[k] = self.gts[k][np.isin(gts_ids[k], intersection_ids)]
-                    
+
             elif self.filling_strategy == NN_FILL_UPSAMPLE and not all_equal:
                 if len(img_ids) < max(list_lengths):
                     raise ValueError("Upsampling is not possible if the dataset is smaller than the biggest folder")
-                
+
                 logging.warning("Upsampling missing labels to fit the dataset's size (%i)" % len(img_ids))
                 for k, values in self.gts.items():
                     temps_ids = np.isin(img_ids, gts_ids[k])
-                    gts_k = self.img_filepath['image'].copy()
+                    gts_k = np.zeros(len(img_ids), dtype=values.dtype)
                     gts_k[temps_ids] = values
                     gts_k[~temps_ids] = MISSING_DATA_FLAG
                     self.gts[k] = gts_k
-            
 
     def load_image(self, item):
         inputs = super(SegmentationDataset, self).load_image(item)
