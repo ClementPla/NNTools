8ee4e8bedcb23ed2e877cc40f86b9e78eda72a3c
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-12-21 15:34:46 -0500

    Fix various and replace the use of str path to pathlib.Path object

--- src/nntools/dataset/image_dataset.py
+++ src/nntools/dataset/image_dataset.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from timeit import default_timer as timer
 from typing import Literal
 
 import numpy as np
@@ -21,24 +22,29 @@ class MultiImageDataset(AbstractImageDataset):
             img_root = {}
             for k, v in self.img_root.items():
                 img_root[k] = to_iterable(v)
-                
-        self.img_filepath = {k: [] for k in img_root.keys()}
 
+        self.img_filepath = {k: [] for k in img_root.keys()}
+        start = timer()
         prefix = "**/*" if recursive else "*"
         for root_label, paths in img_root.items():
             for path in paths:
                 filepaths = [p.resolve() for p in Path(path).glob(prefix) if p.suffix in supportedExtensions]
                 self.img_filepath[root_label].extend(filepaths)
-        
-        if len(self.img_filepath.keys())>1:
+        end = timer()
+        logging.debug(f'Listing files took {end - start}')
+        if len(self.img_filepath.keys()) > 1:
             imgs_ids = {}
+            start = timer()
             for k, filepaths in self.img_filepath.items():
                 self.img_filepath[k] = np.asarray(filepaths)
                 imgs_ids[k] = np.asarray([self.extract_image_id_function(path.stem) for path in self.img_filepath[k]])
                 argsort_ids = np.argsort(imgs_ids[k])
                 imgs_ids[k] = imgs_ids[k][argsort_ids]
                 self.img_filepath[k] = self.img_filepath[k][argsort_ids]
-
+            end = timer()
+            logging.debug(f'Sorting files took {end - start}')
+            
+            start = timer()
             list_lengths = [len(img_ids) for img_ids in imgs_ids.values()]
             all_equal = all(elem == list_lengths[0] for elem in list_lengths)
             if not all_equal:
@@ -51,15 +57,21 @@ class MultiImageDataset(AbstractImageDataset):
             list_common_file = set.intersection(*map(set, list(imgs_ids.values())))
             intersection_ids = np.asarray(list(list_common_file))
             logging.debug(f"Number of files in intersection dataset: {len(intersection_ids)}")
-
+            end = timer()
+            logging.debug(f'Finding common files took {end - start}')
             if self.filling_strategy == NN_FILL_DOWNSAMPLE or all_equal:
+                start = timer()
                 # We only keep the intersection of the files
                 if not all_equal:
                     logging.warning("Downsampling the dataset to size %i" % min(list_lengths))
                 for k, ids in imgs_ids.items():
                     self.img_filepath[k] = self.img_filepath[k][np.isin(ids, intersection_ids)]
+                
+                end = timer()
+                logging.debug(f'Downsampling number of files took {end - start}')
 
             elif self.filling_strategy == NN_FILL_UPSAMPLE and not all_equal:
+                start = timer()
                 union_ids = np.asarray(set.union(*map(set, list(imgs_ids.values)))).sort()
 
                 for k, v in imgs_ids.items():
@@ -68,6 +80,9 @@ class MultiImageDataset(AbstractImageDataset):
                     img_filepath[temps_ids] = self.img_filepath[k]
                     img_filepath[~temps_ids] = MISSING_DATA_FLAG
                     self.img_filepath[k] = img_filepath
+                
+                end = timer()
+                logging.debug(f'Upsampling number of files took {end - start}')
 
     def __len__(self):
         if self.filling_strategy == NN_FILL_DOWNSAMPLE:
