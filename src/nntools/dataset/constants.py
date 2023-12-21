aa8d435300b0d9ad1292a99e416cb1bf447f4091
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-12-21 16:19:58 -0500

    pathlib is way too slow, we go back to os.path

---
+++ src/nntools/dataset/constants.py
@@ -0,0 +1,2 @@
+supportedExtensions = {".jpg", ".jpeg", ".png", ".tiff", ".tif", ".jp2", ".exr", ".pbm", ".pgm", ".ppm", ".pxm", ".pnm"}
+supportedExtensions.update({ext.upper() for ext in supportedExtensions})
